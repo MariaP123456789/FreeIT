67b9ed907fe4e1deb5161e299e3969faf064405e
LocUser <pmyu96@gmail.com>
2022-07-03 16:41:59 +0300

    Task 3.2

--- task3.py
+++ task3.py
@@ -1,3 +1,7 @@
 #3.1 Организовать консольный вывод строки “Hello world!”
 print('Hello, world!')
-
+#3.2 Запросить консольный ввод имени пользователя, организовать консольный вывод строки вида: “Hello, {user}!”, где user – введенное пользователем имя.
+user1 = input()
+print('Hello ' + str(user1))
+print(f'Hello {user1}')
+print('Hello {}'.format(user1))
